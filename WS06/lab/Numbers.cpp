6195da72f1c86b0f9370fa60817a0bdb7064a811
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2023-02-24 11:07:39 -0500

    Workshop 06

---
+++ WS06/lab/Numbers.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include "Numbers.h"
+using namespace std;
+namespace sdds {
+   void Numbers::sort(double* nums, unsigned int size) {
+      unsigned int i, j;
+      double temp;
+      for(i = size - 1; size && i > 0; i--) {
+         for(j = 0; j < i; j++) {
+            if(nums[j] > nums[j + 1]) {
+               temp = nums[j];
+               nums[j] = nums[j + 1];
+               nums[j + 1] = temp;
+            }
+         }
+      }
+   }
+
+   unsigned int Numbers::countLines(const char* filename) { /**/
+      unsigned int lines = 0u;
+      ifstream nums(filename);
+      while(nums) {
+         lines += (nums.get() == '\n');
+      }
+      return lines;
+   }
+}
